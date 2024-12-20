5dbeab6666fbb93ea46af994e9e17baa71fbd818
Keaton Brewster <keaton.brewster@synact.net>
2024-12-19 23:21:50 -0600

    modifying build so that CSS is loaded properly

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 import esbuild from "esbuild";
 import CssModulesPlugin from "esbuild-css-modules-plugin";
+import fs from "fs/promises";
 
 esbuild
   .build({
@@ -7,7 +8,7 @@ esbuild
     outfile: "dist/bundle.js",
     format: "esm",
     tsconfig: "./tsconfig.json",
-    sourcemap: true,
+    sourcemap: false,
     bundle: true,
     platform: "browser",
     target: "esnext",
@@ -15,15 +16,38 @@ esbuild
     plugins: [
       CssModulesPlugin({
         force: true,
-        emitDeclarationFile: true,
-        localsConvention: "camelCaseOnly",
+        emitDeclarationFile: false,
         namedExports: true,
-        inject: false,
       }),
     ],
     loader: {
       ".ts": "ts",
       ".css": "css",
     },
+  })
+  .then(async () => {
+    try {
+      const cssOutputPath = "./dist/bundle.css";
+      // Read the generated CSS file content
+      const cssContent = await fs.readFile(cssOutputPath, "utf-8");
+
+      // Create a JavaScript snippet to inject the CSS into the <head>
+      const cssInjectionCode = `
+          (function() {
+            const style = document.createElement('style');
+            style.textContent = ${JSON.stringify(cssContent)};
+            document.head.appendChild(style);
+          })();
+        `;
+
+      // Append the injection code to the end of the bundle.js
+      const bundlePath = "./dist/bundle.js";
+      const bundleContent = await fs.readFile(bundlePath, "utf-8");
+      await fs.writeFile(bundlePath, `${bundleContent}\n${cssInjectionCode}`);
+
+      console.log("CSS injected successfully!");
+    } catch (error) {
+      console.error("Error injecting CSS:", error);
+    }
   })
   .catch(() => process.exit(1));
