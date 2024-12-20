6c0eb050305285c5a1c265a2979cec71565acd9e
Keaton Brewster <keaton.brewster@synact.net>
2024-12-19 23:22:55 -0600

    moved css import to top of file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /// <reference path="../@types/index.d.ts" />
+import './style.css'
 
 import API from "./API.js";
 import {
   createDOMNodeReference,
   createMultipleDOMNodeReferences,
 } from "./createDOMNodeReferences.js";
-import "./style.css";
 
 export { API, createDOMNodeReference, createMultipleDOMNodeReferences };
